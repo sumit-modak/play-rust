9d9c7674d8e3f87c9955649dcd0c7fad53b4a605
Sumit Modak <142516692+real-sm@users.noreply.github.com>
2023-10-23 23:50:44 +0530

    added monads

---
+++ monads.rs
@@ -0,0 +1,48 @@
+#[derive(Debug)]
+struct NumWithLogs {
+    result: i64,
+    logs: Vec<String>
+}
+
+impl NumWithLogs {
+    fn new(x: i64) -> NumWithLogs {
+        NumWithLogs {
+            result: x,
+            logs: Vec::new()
+        }
+    }
+}
+
+fn run(input: NumWithLogs, transform: &dyn Fn(i64) -> NumWithLogs) -> NumWithLogs {
+    let newNWL = transform(input.result);
+    NumWithLogs {
+        result: newNWL.result,
+        logs: input.logs.append(&mut newNWL.logs)
+    }
+}
+
+// Monads implemented above //
+// Code written below is meant for testing //
+
+fn square(x: i64) -> NumWithLogs {
+    NumWithLogs {
+        result: x * x,
+        logs: vec![format!("Squared {} to get {}", x, x*x)]
+    }
+}
+
+fn addOne(x: i64) -> NumWithLogs {
+    NumWithLogs {
+        result: x + 1,
+        logs: vec![format!("Added 1 to {} to get {}", x, x+1)]
+    }
+}
+
+fn main() {
+    let a = NumWithLogs::new(4);    
+    println!("{:?}", a);
+    let b = run(a, &square);
+    println!("{:?}", b);
+    let c = run(b, &addOne);
+    println!("{:?}", c);
+}
