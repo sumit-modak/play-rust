4b02120613f6ca1eb4bdc8d4f47980ab960dd643
Sumit Modak <142516692+real-sm@users.noreply.github.com>
2023-11-03 23:23:05 +0530

    added string basics

---
+++ iterables/string/string_basics.rs
@@ -0,0 +1,16 @@
+fn main() {  
+   let mut s = String::from("hello, world");
+
+   let slice1: &str = &s; // In two ways
+   assert_eq!(slice1, "hello, world");
+
+   let slice2 = &s[0..5];
+   assert_eq!(slice2, "hello");
+
+   let slice3: &mut String = &mut s; 
+   slice3.push('!');
+   assert_eq!(slice3, "hello, world!");
+
+   println!("Success!");
+}
+
