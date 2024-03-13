30c229e9ecf359019313eb4ce529807bbb3a1cca
Sumit Modak <142516692+sumit-modak@users.noreply.github.com>
2024-03-13 18:53:11 +0530

    few changes

--- struct_impl.rs
+++ struct_impl.rs
@@ -2,16 +2,23 @@ struct Val<T> {
     val: T,
 }
 
+enum Hello {
+    Some(u8),
+    None,
+}
+
 impl<T> Val<T> {
+    #[inline]
     fn value(&self) -> &T {
         &self.val
     }
 }
 
-
 fn main() {
-    let x = Val{ val: 3.0 };
-    let y = Val{ val: "hello".to_string()};
+    let s = "\"hello\"";
+    let x = Val { val: 3.0 };
+    let y = Val {
+        val: "hello".to_string(),
+    };
     println!("{}, {}", x.value(), y.value());
 }
-
